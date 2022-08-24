67ae5af537546b6673ecb9154b00f3cacf847d3d
Daniel Sanches <danielsanches6301@gmail.com>
2022-08-24 17:44:18 -0300

    Add project files.

---
+++ CursoOnline.DominioTest/Cursos/CursoTest.cs
@@ -0,0 +1,116 @@
+﻿using CursoOnline.DominioTest._Util;
+using ExpectedObjects;
+using System;
+using Xunit;
+
+namespace CursoOnline.DominioTest.Cursos
+{
+    public class CursoTest
+    {
+        [Fact]
+        public void DeveCriarCurso()
+        {
+            var cursoEspereado = new
+            {
+                Nome = "Informática básica",
+                CargaHoraria = (double)80,
+                PublicoAlvo = PublicoAlvo.Estudante,
+                Valor = (double)950
+            };
+
+            var curso = new Curso(cursoEspereado.Nome, cursoEspereado.CargaHoraria, cursoEspereado.PublicoAlvo, cursoEspereado.Valor);
+
+            cursoEspereado.ToExpectedObject().ShouldMatch(curso);
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData(null)]
+        public void NaoDeveCursoTerUmNomeInvalido(string nomeInvalido)
+        {
+            var cursoEspereado = new
+            {
+                Nome = "Informática básica",
+                CargaHoraria = (double)80,
+                PublicoAlvo = PublicoAlvo.Estudante,
+                Valor = (double)950
+            };
+
+            Assert.Throws<ArgumentException>(() =>
+                new Curso(nomeInvalido, cursoEspereado.CargaHoraria, cursoEspereado.PublicoAlvo, cursoEspereado.Valor))
+                .ComMensagem("Nome inválido");
+        }
+
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(-2)]
+        public void NaoDeveCursoTerUmaCargaHorariaMenorQue1(double cargaHorariaInvalida)
+        {
+            var cursoEspereado = new
+            {
+                Nome = "Informática básica",
+                CargaHoraria = (double)80,
+                PublicoAlvo = PublicoAlvo.Estudante,
+                Valor = (double)950
+            };
+
+            Assert.Throws<ArgumentException>(() =>
+                new Curso(cursoEspereado.Nome, cargaHorariaInvalida, cursoEspereado.PublicoAlvo, cursoEspereado.Valor))
+                .ComMensagem("Carga horária inválida");
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(-2)]
+        public void NaoDeveCursoTerValorMenorQue1(double valorInvalido)
+        {
+            var cursoEspereado = new
+            {
+                Nome = "Informática básica",
+                CargaHoraria = (double)80,
+                PublicoAlvo = PublicoAlvo.Estudante,
+                Valor = (double)950
+            };
+
+            Assert.Throws<ArgumentException>(() =>
+                new Curso(cursoEspereado.Nome, cursoEspereado.CargaHoraria, cursoEspereado.PublicoAlvo, valorInvalido))
+                .ComMensagem("Valor inválido");
+        }
+    }
+
+    public enum PublicoAlvo
+    {
+        Estudante,
+        Universitário,
+        Empregado,
+        Empreendedor
+    }
+
+
+    public class Curso
+    {
+        public string Nome { get; private set; }
+        public double CargaHoraria { get; private set; }
+        public PublicoAlvo PublicoAlvo { get; private set; }
+        public double Valor { get; private set; }
+
+        public Curso(string nome, double cargaHoraria, PublicoAlvo publicoAlvo, double valor)
+        {
+            if (string.IsNullOrEmpty(nome))
+                throw new ArgumentException("Nome inválido");
+
+
+            if (cargaHoraria < 1)
+                throw new ArgumentException("Carga horária inválida");
+
+            if (valor < 1)
+                throw new ArgumentException("Valor inválido");
+
+            Nome = nome;
+            CargaHoraria = cargaHoraria;
+            PublicoAlvo = publicoAlvo;
+            Valor = valor;
+        }
+    }
+}
---
+++ src/CursoOnline.Dominio/Curso.cs
@@ -0,0 +1,18 @@
+﻿namespace CursoOnline.Dominio
+{
+    public class Curso
+    {
+        //    public string Nome { get; private set; }
+        //    public double CargaHoraria { get; private set; }
+        //    public PublicoAlvo PublicoAlvo { get; private set; }
+        //    public double Valor { get; private set; }
+
+        //    public Curso(string nome, double cargaHoraria, PublicoAlvo publicoAlvo, double valor)
+        //    {
+        //        Nome = nome;
+        //        CargaHoraria = cargaHoraria;
+        //        PublicoAlvo = publicoAlvo;
+        //        Valor = valor;
+        //    }
+    }
+}
